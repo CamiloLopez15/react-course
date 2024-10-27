b39f3e15b92c966a9a50a25ad7f101d64f89c33c
Camilo López <91794023+CamiloLopez15@users.noreply.github.com>
2024-10-26 22:00:26 -0500

    feat: added classes of 52 to 57

--- 03-counter-app-vite/test/demo.test.ts
+++ 03-counter-app-vite/test/demo.test.ts
@@ -1,5 +1,15 @@
-test('this test should not fail', () => {
+describe("Pruebas del Jest", () => {
+    test("this test should not fail", () => {
+        //? 1. Inicialización - Arrange.
 
-})
+        const message1 = "Hola mundo";
 
+        //? 2. Estimulo - Arrange.
 
+        const message2 = message1.trim();
+
+        //? 3. Observar el comportamiento - Assert.
+
+        expect(message1).toBe(message2);
+    });
+});
