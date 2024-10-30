fc72e4422299429ee2bbdc823c5fa56cce4f3718
Camilo López <91794023+CamiloLopez15@users.noreply.github.com>
2024-10-30 18:43:02 -0500

    feat: added class number 61

--- 03-counter-app-vite/src/base-pruebas/11-async-await.js
+++ 03-counter-app-vite/src/base-pruebas/11-async-await.js
@@ -1,27 +1,19 @@
-
-export const getImagen = async() => {
-
+export const getImagen = async () => {
     try {
-
-        const apiKey = 'rmKHcEx9HqMUxZ1ubUVugb1mTOdVmiO0';
-        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-        const { data } = await resp.json(); 
+        const apiKey = "4Qp9NceWTWV3NvVHPxGJTzRIWqPbjE9B";
+        const resp = await fetch(
+            `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+        );
+        const { data } = await resp.json();
 
         const { url } = data.images.original;
 
         return url;
-
     } catch (error) {
         // manejo del error
         // console.error(error)
-        return 'No se encontro la imagen'
+        return "No se encontro la imagen";
     }
-    
-    
-    
-}
+};
 
- getImagen();
-
-
-
+getImagen();
