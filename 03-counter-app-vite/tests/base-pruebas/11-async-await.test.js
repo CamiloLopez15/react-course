e65a05455d2923d3643a903b02f2db0df601871b
Camilo López <91794023+CamiloLopez15@users.noreply.github.com>
2024-10-30 18:48:12 -0500

    feat: added class number 61

--- 03-counter-app-vite/tests/base-pruebas/11-async-await.test.js
+++ 03-counter-app-vite/tests/base-pruebas/11-async-await.test.js
@@ -2,7 +2,10 @@ import { getImagen } from "../../src/base-pruebas/11-async-await";
 
 describe("Pruebas en 11-async-await.js", () => {
     test("getImagen debe de retornar un error si no tenemos api key", async () => {
-        const resp = await getImagen();
-        expect(typeof resp).toBe("string")
+        try {
+            const resp = await getImagen();
+            // expect(typeof resp).toBe("string");
+            expect(resp).toBe("No se encontró la imagen");
+        } catch (error) {}
     });
 });
