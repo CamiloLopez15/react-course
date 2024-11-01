65d29d7917738b5a2de7458b1bb0d801de3866f1
Camilo López <91794023+CamiloLopez15@users.noreply.github.com>
2024-10-31 21:38:24 -0500

    feat: added class number 68

--- 03-counter-app-vite/tests/CounterApp.test.jsx
+++ 03-counter-app-vite/tests/CounterApp.test.jsx
@@ -1,3 +1,18 @@
-describe('Pruebas en el componente CounterApp', () => {
-  
-})
+import { render, screen } from "@testing-library/react";
+import { CounterApp } from "../src/CounterApp";
+
+describe("Pruebas en el componente CounterApp", () => {
+    const initialValue = 100;
+    test("El componente debe hacer  match con el snapshot", () => {
+        const { container } = render(<CounterApp value={initialValue} />);
+        expect(container).toMatchSnapshot();
+    });
+    test(`Debe mostrar el valor inicial en ${initialValue}`, () => {
+        render(<CounterApp value={initialValue} />);
+        expect(screen.getByText(initialValue)).toBeTruthy();
+        //? OR
+        expect(screen.getByRole("heading", { level: 2 }).innerHTML).toContain(
+            String(initialValue)
+        );
+    });
+});
