f9116dbcf2ffb36995338ad0b66387fd34580056
Camilo López <camilolopez1506@gmail.com>
2024-12-01 11:20:34 -0500

    feat: added class number 170

---
+++ 05-basics-hooks/test/03-examples/MultipleCustomHooks.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { MultipleCustomHooks } from "../../src/03-example";
+
+describe("Pruebas en el componente MultipleCustomHooks", () => {
+    test("debe de mostrar el componente por defecto", () => {
+        render(<MultipleCustomHooks />);
+        expect(screen).toMatchSnapshot();
+        expect(screen.getByText("Información de Pokemon")).toBeTruthy();
+        expect(screen.getByText("Cargando...")).toBeTruthy();
+
+
+        screen.debug();
+    });
+});
