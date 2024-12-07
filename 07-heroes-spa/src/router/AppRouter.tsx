ce2f2df2414c9a347a4b3292930564e9cd2b53dc
Camilo López <91794023+CamiloLopez15@users.noreply.github.com>
2024-12-06 20:04:03 -0500

    feat: added class number 192

--- 07-heroes-spa/src/router/AppRouter.tsx
+++ 07-heroes-spa/src/router/AppRouter.tsx
@@ -1,16 +1,20 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Marvel } from "@heroes/pages/Marvel";
-import { Login } from "@auth/pages/Login";
-import { Dc } from "@heroes/pages/Dc";
+import { Dc, Marvel } from "@src/heroes";
+import { Navbar } from "@src/ui";
+import { Login } from "@src/auth";
+
 
 export const AppRouter = () => {
     return (
-        <Routes>
-            <Route path="/marvel" element={<Marvel />} />
-            <Route path="/dc" element={<Dc />} />
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/marvel" element={<Marvel />} />
+                <Route path="/dc" element={<Dc />} />
 
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Navigate to="/marvel"/>} />
-        </Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Navigate to="/marvel" />} />
+            </Routes>
+        </>
     );
 };
