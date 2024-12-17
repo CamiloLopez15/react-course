45c3ffe64cc239d9507895c1cb8c9a0929a4dc99
Camilo López <91794023+CamiloLopez15@users.noreply.github.com>
2024-12-16 21:00:21 -0500

    feat: added class number 209

--- 07-heroes-spa/src/heroes/pages/Search.tsx
+++ 07-heroes-spa/src/heroes/pages/Search.tsx
@@ -1,23 +1,28 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import { FormEvent, useEffect } from "react";
-import { useForm } from "@src/hooks/useForm";
+import { getHeroesByName } from "../helpers";
+import { FormEvent } from "react";
 import queryString from "query-string";
-// import { HeroCard } from "./../components";
+import { useForm } from "@src/hooks/useForm";
+import { HeroCard } from "../components";
 
 export const Search = () => {
     const navigate = useNavigate();
-    const { search } = useLocation();
-    const { q = "" } = queryString.parse(search);
+    const location = useLocation();
+
+    const params = queryString.parse(location.search);
+    const q: string = (params.q as string) || undefined || "";
+    const heroes = getHeroesByName(q);
+
+    const showSearch = q.length === 0;
+    const showError = q.length > 0 && heroes.length === 0;
 
     const { searchText, onChangeInput } = useForm({
-        searchText: "",
+        searchText: q,
     });
-    useEffect(() => {}, [search]);
 
-    const onSearchSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (searchText.trim().length <= 1) return;
-        navigate(`q=${searchText.toLowerCase().trim()}`);
+    const onSearchSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        navigate(`?q=${searchText}`);
     };
 
     return (
@@ -32,14 +37,14 @@ export const Search = () => {
                         <input
                             type="text"
                             placeholder="Search a hero"
-                            className="form-control"
+                            className="form-control rounded-0 rounded-start"
                             name="searchText"
                             autoComplete="off"
                             value={searchText}
                             onChange={onChangeInput}
                         />
 
-                        <button className="btn btn-outline-primary mt-1">
+                        <button className="btn btn-outline-primary rounded-0 rounded-end">
                             Search
                         </button>
                     </form>
@@ -48,12 +53,25 @@ export const Search = () => {
                 <div className="col-7">
                     <h4>Result</h4>
                     <hr />
-                    <div className="alert alert-primary">Search a Hero</div>
-                    <div className="alert alert-danger">
+                    <div
+                        className="alert alert-primary animate__animated animate__fadeIn"
+                        style={{ display: showSearch ? "" : "none" }}
+                    >
+                        Search a hero
+                    </div>
+
+                    <div
+                        className="alert alert-danger animate__animated animate__fadeIn"
+                        style={{ display: showError ? "" : "none" }}
+                    >
                         No hero with <b>{q}</b>
                     </div>
 
-                    {/* <HeroCard /> */}
+                    <div className="d-flex flex-column gap-2">
+                        {heroes.map((hero) => (
+                            <HeroCard hero={hero} key={hero.id} />
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
