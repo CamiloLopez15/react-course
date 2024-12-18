344876e78c2cb1804b46f95bc215cedb96d0329e
Camilo López <91794023+CamiloLopez15@users.noreply.github.com>
2024-12-17 22:44:57 -0500

    feat: added class number 213

--- 07-heroes-spa/src/auth/context/AuthProvider.tsx
+++ 07-heroes-spa/src/auth/context/AuthProvider.tsx
@@ -1,24 +1,51 @@
+import { ActionAuthReducer, Types, User } from "../types";
 import { ReactNode, useReducer } from "react";
 import { initialState } from "../data/user";
 import { AuthContext } from "./";
 import { authReducer } from "./";
-import { ActionAuthReducer, Types } from "../types";
 
 interface AuthProvider {
     children: ReactNode;
 }
 
+const initAuth = (initialState: User) => {
+    const data = localStorage.getItem("user");
+    if (data) {
+        const user: User = JSON.parse(data);
+        return user;
+    }
+
+    return initialState;
+};
+
+const setItem = (user: User) => {
+    try {
+        localStorage.setItem("user", JSON.stringify(user));
+    } catch (error) {
+        console.error(error);
+    }
+};
+
 function AuthProvider({ children }: AuthProvider) {
-    const [authState, dispatchAuth] = useReducer(authReducer, initialState);
+    const [authState, dispatchAuth] = useReducer(
+        authReducer,
+        initialState,
+        initAuth
+    );
 
     const login = async (name: string) => {
+        const newUser = {
+            id: "ABC",
+            username: name,
+        };
+
         const actions: ActionAuthReducer = {
             type: Types.login,
-            payload: {
-                id: "ABC",
-                username: name,
-            },
+            payload: newUser,
         };
+
+        setItem(Object.assign(authState, actions));
+
         dispatchAuth(actions);
     };
 
