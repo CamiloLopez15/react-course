e6b2246acb2053ba24f167ef59a4a639b8f0a5a7
Camilo López <91794023+CamiloLopez15@users.noreply.github.com>
2025-01-12 19:00:05 -0500

    feat: added class 247

--- 08-journal-app/src/auth/pages/RegisterPage.tsx
+++ 08-journal-app/src/auth/pages/RegisterPage.tsx
@@ -1,5 +1,79 @@
+import { Button, Grid2, Link, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router";
+import AuthLayout from "../layout/AuthLayout";
+
 function RegisterPage() {
-    return <div>RegisterPage</div>;
+    return (
+        <AuthLayout headerText="Registro">
+            <form>
+                <Grid2 container>
+                    <Grid2
+                        size={12}
+                        sx={{
+                            mt: 2,
+                        }}
+                    >
+                        <TextField
+                            label="Nombre completo"
+                            type="text"
+                            placeholder="Nombre completo"
+                            fullWidth
+                        />
+                    </Grid2>
+
+                    <Grid2
+                        size={12}
+                        sx={{
+                            mt: 2,
+                        }}
+                    >
+                        <TextField
+                            label="Correo"
+                            type="email"
+                            placeholder="correo@gmail.com"
+                            fullWidth
+                        />
+                    </Grid2>
+
+                    <Grid2
+                        size={12}
+                        sx={{
+                            mt: 2,
+                        }}
+                    >
+                        <TextField
+                            label="Contraseña"
+                            type="password"
+                            placeholder="Contraseña"
+                            fullWidth
+                        />
+                    </Grid2>
+
+                    <Grid2
+                        container
+                        spacing={2}
+                        sx={{ mb: 2, mt: 1, width: "100%" }}
+                    >
+                        <Grid2 size={12}>
+                            <Button variant="contained" fullWidth>
+                                Crear cuenta
+                            </Button>
+                        </Grid2>
+                    </Grid2>
+                </Grid2>
+                <Grid2 container direction="row" justifyContent="end">
+                    <Typography mr={1}>¿Ya tienes cuenta?</Typography>
+                    <Link
+                        color="inherit"
+                        to="/auth/login"
+                        component={RouterLink}
+                    >
+                        ingresar
+                    </Link>
+                </Grid2>
+            </form>
+        </AuthLayout>
+    );
 }
 
 export default RegisterPage;
