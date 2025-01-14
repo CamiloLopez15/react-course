38befe8af89a6fc51c4423fd175052e70a418723
Camilo López <91794023+CamiloLopez15@users.noreply.github.com>
2025-01-14 18:16:59 -0500

    eat: added class 254

--- 08-journal-app/src/journal/pages/JournalPage.tsx
+++ 08-journal-app/src/journal/pages/JournalPage.tsx
@@ -1,12 +1,33 @@
-// import NothingSelectedView from "../views/NothingSelectedView";
+import NothingSelectedView from "../views/NothingSelectedView";
 import JournalLayout from "../layout/JournalLayout";
-import NoteView from "../views/NoteView";
+// import NoteView from "../views/NoteView";
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
 
 function JournalPage() {
     return (
         <JournalLayout>
-            {/* <NothingSelectedView /> */}
-            <NoteView />
+            <NothingSelectedView />
+            {/* <NoteView /> */}
+
+            <IconButton
+                size="large"
+                sx={{
+                    color: "white",
+                    backgroundColor: "error.main",
+                    position: "fixed",
+                    bottom: 50,
+                    right: 50,
+                    transitionDuration: "0.3s",
+                    transitionProperty: "all",
+                    ":hover": {
+                        backgroundColor: "error.main",
+                        opacity: 0.9,
+                    },
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }} />
+            </IconButton>
         </JournalLayout>
     );
 }
