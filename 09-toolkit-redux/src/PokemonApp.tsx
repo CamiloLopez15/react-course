489da4dacb65b3b16fb1da891666cb148dd3f336
Camilo López <91794023+CamiloLopez15@users.noreply.github.com>
2025-01-16 21:59:55 -0500

    feat: added class 268

--- 09-toolkit-redux/src/PokemonApp.tsx
+++ 09-toolkit-redux/src/PokemonApp.tsx
@@ -1,25 +1,33 @@
 import { useEffect } from "react";
-import { useAppDispatch } from "./store/hooks";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
 import { getPokemons } from "./store/slices/pokemon/thunks";
 
 function PokemonApp() {
     const dispatch = useAppDispatch();
-
+    const { isLoading, pokemons, page } = useAppSelector(
+        (state) => state.pokemon
+    );
     useEffect(() => {
         dispatch(getPokemons(0));
     }, [dispatch]);
+
+    const handleNext = () => {
+        dispatch(getPokemons(page));
+    };
 
     return (
         <>
             <h1>Pokemon App</h1>
             <hr />
-
+            <span>Loading: {isLoading ? "True" : "False"}</span>
             <ul>
-                <li>Hola</li>
-                <li>Hola</li>
-                <li>Hola</li>
-                <li>Hola</li>
+                {pokemons.map(({ name, id }) => (
+                    <li key={id}>{name}</li>
+                ))}
             </ul>
+            <button disabled={isLoading} onClick={handleNext}>
+                Next
+            </button>
         </>
     );
 }
